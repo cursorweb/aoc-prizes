ef1ee9b3bf890d2308e1f771e2ab1a9e31d60d51
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-12-09 19:30:48 -0600

    added a basic help

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Ping 10 random people at a time of a day, and the first that responds wins
 **How to respond?**
 * React to the message
 
-Invite link: [here](https://discord.com/api/oauth2/authorize?client_id=1183176323242414220&permissions=2048&scope=bot)
+Invite link: [here](https://discord.com/api/oauth2/authorize?client_id=1183176323242414220&permissions=3072&scope=applications.commands+bot)
 
 > Don't forget the members intent or it will be unfair. And also fetch the members instead of relying on the cache.
--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const token = process.env.TOKEN;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
+/*
+guild id: 575842109672652822
+channel id: 867088236865257492
+*/
+
+const guildId = "575842109672652822";
+const channelId = "867088236865257492";
+
+client.once(Events.ClientReady, async readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    // console.log(readyClient.guilds.cache.get("575842109672652822").channels.cache.get("867088236865257492").send("hello"))
+    const guild = await client.guilds.fetch(guildId);
+    const channel = await guild.channels.fetch(channelId);
+    channel.send("hello world");
 });
 
 client.on(Events.MessageCreate, msg => {
     if (msg.author.bot) return;
 
-    msg.channel.send("hello, world!");
+    if (msg.mentions.has(msg.client.user.id)) {
+        msg.channel.send(`You can invite me [here](https://discord.com/api/oauth2/authorize?client_id=1183176323242414220&permissions=3072&scope=applications.commands+bot)
+You can also view the github: [here](https://github.com/cursorweb/aoc-prizes-bot)`);
+    }
 });
 
 // Log in to Discord with your client's token
