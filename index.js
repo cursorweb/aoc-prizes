c57bfdcfe4502e264215e3020814fdc52c94ea78
Junhao Zhang <jerry_mountain@hotmail.com>
2023-12-19 10:46:30 -0600

    make gollector

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const token = process.env.TOKEN;
 
@@ -16,21 +15,49 @@ channel id: 867088236865257492
 const guildId = "575842109672652822";
 const channelId = "867088236865257492";
 
-client.once(Events.ClientReady, async readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+/**
+ * gets the #bot-commands channel
+ * @returns the channel
+ */
+async function getChannel() {
     const guild = await client.guilds.fetch(guildId);
     const channel = await guild.channels.fetch(channelId);
-    channel.send("hello world");
+    return channel;
+}
+
+client.once(Events.ClientReady, async readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.MessageCreate, msg => {
+client.on(Events.MessageCreate, async msg => {
     if (msg.author.bot) return;
 
     if (msg.mentions.has(msg.client.user.id)) {
         msg.channel.send(`You can invite me [here](https://discord.com/api/oauth2/authorize?client_id=1183176323242414220&permissions=3072&scope=applications.commands+bot)
-You can also view the github: [here](https://github.com/cursorweb/aoc-prizes-bot)`);
+You can also view the github: [here](https://github.com/cursorweb/aoc-prizes-bot)
+
+**Commands:**
+\`~gen\` prompt user to generate a random time
+\`~update\` update the site (todo)
+\`~test\` send a message`);
+    }
+
+    let cmd = msg.content.toLowerCase().trim();
+    if (cmd == "~gen") {
+        // good luck!!
+        msg.reply({ content: "What time to <verb>?" })
+            .then(() => {
+                const filter = _ => true;
+                msg.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
+                    .then(collected => {
+                        // console.log(collected);
+                        msg.reply(`we have: ${collected.first().content}`);
+                    })
+                    .catch(collected => {
+                        msg.reply('Looks like nobody got the answer this time.');
+                    });
+            });
     }
 });
 
-// Log in to Discord with your client's token
 client.login(token);
